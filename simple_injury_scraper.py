0cc9ee713440694e0137ee73cd387cc6109c8989
Jake Hari <hari.jake1@gmail.com>
2025-09-11 06:39:53 -0500

    fix syntax error - add test data

--- simple_injury_scraper.py
+++ simple_injury_scraper.py
@@ -1,7 +1,10 @@
-import json 
- 
-def scrape_hockey_reference_injuries(): 
-    try: 
-        return [] 
-    except: 
-        return []   git add .
+def scrape_hockey_reference_injuries():
+    return [
+        {
+             'player': 'Sample Player',
+             'team': 'NYR',
+             'injury_type': 'Upper Body',
+             'injury_date': '2025-09-11',
+             'source': 'Test Data'
+        }
+     ]
